37f6fb6efa65408a0bad5eebc4af701467072564
ivontsvetomirovafg <ivontsvetomirova.fg@gmail.com>
2026-04-09 17:45:36 +0200

    Script del enemy (base, no defnitivo)

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,16 +1,119 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float life;
+    [SerializeField]
+    private float attackRange;
+    [SerializeField]
+    public float damage;
+    [SerializeField]
+    private float attackCooldown;
+    private Transform targetPlayer;
+    private float attackTimer;
+    private bool Muerto;
+
+    private CharacterController player;
+    private LevelManager levelManager;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        targetPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+
+        player = FindObjectOfType<CharacterController>();
+        levelManager = FindObjectOfType<LevelManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Muerto == true)
+        {
+            return;
+        }
+        /*if (targetPlayer.GetComponent<CharacterController>().isDead == true)
+        {
+            agent.isStopped = true;
+            animator.SetBool("Iddle", true);
+            return;
+        }*/
+
+        if (targetPlayer == null)
+        {
+            targetPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+            if (targetPlayer == null)
+            {
+                return;
+            }
+        }
+
+        agent.SetDestination(targetPlayer.position);
+        agent.speed = speed;
+
+        float distance = Vector3.Distance(transform.position, targetPlayer.position);
+
+        if (distance <= attackRange)
+        {
+            agent.isStopped = true;
+            Attack();
+        }
+        else
+        {
+            agent.isStopped = false;
+        }
+
+        //cooldown
+
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+        }
+    }
+
+    private void Attack()
+    {
+        if (attackTimer > 0)
+        {
+            return;
+        }
+        //animator.SetTrigger("Attack");
+        attackTimer = attackCooldown;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //collision.gameObject.GetComponent<CharacterController>().TakeDamage(damage);
+        }
+    }
+    public void TakeDamage(float _damage)
+    {
+        Debug.Log("Recibe da�o");
+        life -= _damage;
+
+        if (life <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        agent.Stop();
+        agent.isStopped = true;
+        Muerto = true;
+        //animator.SetTrigger("Death");
+ 
+        GetComponent<Collider>().enabled = false;
+        Destroy(gameObject, 2f);
     }
 }
